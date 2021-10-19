b37fa0e7eb7d9f097c2c49360d1f27b63084743f
FLU <valerio.massj@gmail.com>
2021-10-19 13:38:45 +0200

    initial idea

---
+++ script.js
@@ -0,0 +1,49 @@
+/* Richiesta base:
+- implementare un layout assegnato come esercitazione pomeridiana */
+
+/* Apro Slack */
+
+/*
+    ? SE la repo e' presente nella comunicazione
+        -copio il nome della repo
+
+    : ALTRIMENTI 
+        -torno a giocare a Dota2
+*/
+
+/* Apro GitHub */
+/* Creo la nuova Repo */
+/* Incollo il nome della Repo */
+
+/* Apro VSCode */
+/* Clono la Repo nella cartella degli esercizi */
+/* Creo il file HTML, CSS */
+
+/*
+    ? SE Ho bisogno di implementare le immagini nel progetto
+        -creo la cartella "img"
+
+        ? SE ho a disposizione le immagini
+            -le scarico via slack
+            -le inserisco nella cartella "img" del mio progetto
+
+        : ALTRIMENTI 
+            -le cerco su google
+            -le scarico
+            -le inserisco nella cartella "img" del mio progetto
+
+    : ALTRIMENTI
+        -non creo altre cartelle
+*/
+
+/* Nel file HTML faccio compilare automaticamente il formato base */
+/* Creo il link per il file css */
+
+/* Apro il file html sul browser */
+
+/* Inizio a creare i macroblocchi e sezioni */
+
+/* FINCHE i file html e css sono salvati
+        -controllo le modifiche apportare nel browser
+*/
+    
